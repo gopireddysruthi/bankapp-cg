31413f289f819236ed899f785d00344b11a3db77
unknown <sruthigopireddy25@gmail.com>
2022-01-09 20:12:04 +0530

    initial commit

---
+++ src/test/java/com/springboot/bankapp1/BankApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bankapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
